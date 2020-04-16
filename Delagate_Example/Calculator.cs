460c1713af0a2f11bfe05bea93d8175310cf47a6
cdiazarze <car.diaz@outlook.cl>
2020-04-16 14:24:54 -0400

    first commit

---
+++ Delagate_Example/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delagate_Example
+{
+
+    
+    public static class Calculator
+    {
+        public delegate double CalcCriteria(double n1, double n2);
+        public static double calc(double a, double b, CalcCriteria criteria)
+        {
+            return criteria(a, b);
+        }
+    }
+}
---
+++ Delagate_Example/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delagate_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calculator.CalcCriteria myCriteria = new Calculator.CalcCriteria(Mul);
+            Console.WriteLine(myCriteria(3,10));
+        }
+
+        static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+        static double Sus(double a, double b)
+        {
+            return a - b;
+        }
+        static double Mul(double a, double b)
+        {
+            return a * b;
+        }
+        static double Div(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
